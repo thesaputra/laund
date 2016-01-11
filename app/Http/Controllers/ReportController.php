53d687232a3f7ba9cd99d8b6ef2d0599e3a5f423
Aji Saputra <ajisaputra@Ajis-MacBook-Pro.local>
2016-01-11 13:45:28 +0700

    fix date

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -111,7 +111,7 @@ class ReportController extends Controller
 
   public function get_data_report_status($date_start,$date_end)
   {
-    $date_end = Carbon::parse($date_end)->addDays(1);
+    // $date_end = Carbon::parse($date_end)->addDays(1);
     $results = \DB::select(\DB::raw("select transactions.date_order as trans_date_order, transactions.invoice_number as trans_invoice, customers.name as cust_name,customers.address as cust_address,
                                transaction_details.package_type as trans_detail_type, transactions.id as trans_id, transactions.amount_left as trans_amount_total,
                               (SELECT
