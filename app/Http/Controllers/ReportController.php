1d062000cf047ff8d92ca41c5f6467aa3716698e
Aji Saputra <ajisaputra@Ajis-MacBook-Pro.local>
2016-02-01 11:06:07 +0700

    update

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -223,7 +223,7 @@ class ReportController extends Controller
     ->join('status','transactions.status_id','=','status.id')
 
     ->select('transaction_details.qty as jml_kg','packages.unit as unit_satuan','payment_histories.amount as amount_payment','payment_histories.description as desc_payment','payment_histories.created_at as created_at_payment', 'transactions.invoice_number','transactions.date_checkout','transactions.discount','status.name as status_trans','transactions.date_order',
-             'transactions.description','customers.name as customer_name','customers.address as customer_address','transaction_details.package_type as tipe_paket','packages.price_regular as harga_regular','packages.price_express as harga_express'
+             'payment_histories.description','customers.name as customer_name','customers.address as customer_address','transaction_details.package_type as tipe_paket','packages.price_regular as harga_regular','packages.price_express as harga_express'
 
 
              )
