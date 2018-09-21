358d978d1a7f2bd62ffc8ca52f9ceb5d1f5ca50e
aji saputra <aji.saputra@wgs.co.id>
2018-09-21 18:49:06 +0700

    update

--- resources/views/transactions/print_invoice.blade.php
+++ resources/views/transactions/print_invoice.blade.php
@@ -16,7 +16,7 @@ Hp: 0857 9444 0447
 </address>
 <span>---------------------------------------------------</span>
 <br/>
-<span style="font-size:13px" class="span-header">No.{{$invoice}}</span><br/><span style="font-size:13px" class="span-header">Order: {{ date('d-m-Y', strtotime($data['transaction']->date_order))}}</span> ---
+<span style="font-size:13px" class="span-header">No.{{$invoice}}</span><br/><span style="font-size:13px" class="span-header">Order: {{ date('d-m-Y', strtotime($data['transaction']->date_order))}}</span> <br/>
 <span style="font-size:13px" class="span-header">Selesai: {{ date('d-m-Y', strtotime($data['transaction']->date_deliver))}}</span><br/>
 <span style="font-size:13px" class="span-header">Nama: {{strtoupper($data['transaction']->name.' / '.$data['transaction']->address)}}</span><br/>
 <br/>
